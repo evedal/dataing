c275998dbff14ded3fcbdfd8a948682aa7d23659
evendalen <even.dalen@live.no>
2016-10-06 09:32:39 +0200

    Ferdig øving 7

--- algoritmer_og_datastrukturer/algdat_øving7/algdat_øving7.py
+++ algoritmer_og_datastrukturer/algdat_øving7/algdat_øving7.py
@@ -24,10 +24,16 @@ class Prev():
     def find_dist(self):
         return self.dist
 
-
-
     def __init__(self):
         self.dist = self.infinity
+
+class Topo_1st():
+    found = None
+    next = None
+    id = None
+    def __init__(self,id):
+        self.id = id
+
 
 class Graph():
     N = None
@@ -39,6 +45,7 @@ class Graph():
             self.nodes[i].d = Prev()
         s.d.dist = 0
 
+    #Opens file, formats input and adds to class variables
     def read_file(self,filename):
         file = open(filename,'r')
         info = file.readline().split(" ")
@@ -47,19 +54,13 @@ class Graph():
         self.nodes = []
         for i in range(int(self.N)):
             self.nodes.append(Node(i))
-
-        for i in range(self.K):
-            line = file.readline().split(" ")
-
-            fratil = []
-            for n in line:
-                if n is not '':
-                    fratil.append(n.replace("\n",""))
-            fra = int(fratil[0])
-            til = int(fratil[1])
-            e = Edge(self.nodes[til],self.nodes[fra].edge1)
-            self.nodes[fra].edge1 = e
-
+        lines = file.read().split("\n")
+        for line in lines:
+            if line:
+                l = list(filter(None, line.split(" "))) #filters out input equal to none (empty strings) and returns a list
+                fra, til = list(map(int,l))
+                e = Edge(self.nodes[til],self.nodes[fra].edge1)
+                self.nodes[fra].edge1 = e
         return self.nodes
 
 
@@ -71,7 +72,7 @@ class Graph():
         while len(queue) != 0:
             n = queue.popleft()
             e = n.edge1
-            while e.next is not None:
+            while e is not None:
                 f = e.end.d
                 if f.dist == f.infinity:
                     f.dist = n.d.dist + 1
@@ -81,19 +82,53 @@ class Graph():
         return self.nodes
 
 
+    def df_topo(self,n,l):
+        nd = n.d
+        if(nd.found):return l
+        nd.found = True
+        e = n.edge1
+        while e:
+            l = self.df_topo(e.end,l)
+            e = e.next
+        nd.next = l
+        return n
+
+    def topologisort(self):
+        l = None
+        count = 0
+        for node in reversed(self.nodes):
+            node.d = Topo_1st(count)
+            count += 1
+        for i in reversed(range(self.N)):
+            l = self.df_topo(self.nodes[i],l)
+
+        return l
 
 
-def main():
+
+def main2():
     nodenummer = 5
-    filnavn = "L7g1.txt"
+    filnavn = "L7g5.txt"
     graph = Graph()
-    nodes = graph.read_file(filnavn)
+    n = graph.read_file(filnavn)
+    l = graph.topologisort()
+    print("Node")
+    while l:
+        print(l.d.id)
+        l = l.d.next
+
+def main1():
+    nodenummer = 5
+    filnavn = "L7g3.txt"
+    graph = Graph()
+    n = graph.read_file(filnavn)
     nodes = graph.bfs(nodenummer)
+
     print("Node | Forgj | Dist")
     for i in range(len(nodes)):
         if nodes[i].d.prev is not None:
             print(i,"   |  ",nodes[i].d.prev.id,"  |  ",nodes[i].d.dist)
 
 
-
-main()
+#main1()
+main2()
